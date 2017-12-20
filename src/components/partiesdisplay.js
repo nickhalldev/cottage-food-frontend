cb5bf3721558716dd5fdabbe5a82d439d094e93d
Nick Hall <nickhall122@gmail.com>
2017-12-19 23:12:03 -0500

    done for the night

--- src/components/partiesdisplay.js
+++ src/components/partiesdisplay.js
@@ -3,12 +3,38 @@ import * as actions from "../actions/index"
 import { connect } from 'react-redux'
 import { withRouter } from "react-router-dom"
 
-const MyParties = () => {
+const MyParties = (props) => {
+
+  let bakerVariable = props.baker_transactions ? (
+        <div>
+        <h1>Baker Transactions</h1>
+          {props.baker_transactions.map((transact, index) =>{
+            return <div key={index}>
+            Delivery Date {transact.delivery_date}
+            Delivery Time {transact.delivery_time}
+            Total Cost {transact.total_cost}</div>
+          })}
+        </div>
+    ) : null
+
+    let purchaserVariable = props.purchaser_transactions ? (
+          <div>
+            <h1>Purchaser Transactions</h1>
+            {props.purchaser_transactions.map((transact, index) =>{
+              return <div key={index}>
+              Delivery Date {transact.delivery_date}
+              Delivery Time {transact.delivery_time}
+              Total Cost {transact.total_cost}
+              </div>
+            })}
+          </div>
+      ) : null
 
   return(
     <div>
 
-    Hi Hi partiessssss!
+  {bakerVariable}
+  {purchaserVariable}
 
     </div>
   )
@@ -16,9 +42,10 @@ const MyParties = () => {
 }
 
 const mapStateToProps = state => {
-  // console.log('state.user in map state',state)
+   console.log('state.user in map state',state)
   return {
-    current_user: state.users.current_user
+    baker_transactions: state.users.current_user.baker_transactions,
+    purchaser_transactions: state.users.current_user.purchaser_transactions
   }
 }
 
--- src/components/recipesdisplay.js
+++ src/components/recipesdisplay.js
@@ -5,54 +5,33 @@ import { withRouter } from "react-router-dom"
 const url = "http://localhost:3001/api/v1/"
 // const RecipeData = {}
 
-class MyRecipes extends React.Component {
-  constructor(props){
-    super(props)
-    // console.log('this is my props',props)
+const MyRecipes = (props) => {
 
-    this.state = {
-      // users: [],
-      // user: {},
-    }
-  }
+let recipeVariable = props.recipes ? (
+      <div>
+      <h1>My Saved Recipes</h1>
+        {props.recipes.map((recipe, index) =>{
+          return <div key={index}> {recipe.name} </div>
+        })}
+      </div>
+  ) : null
 
-componentDidMount(){
-  // this.fetchAllUserData()
+return(
+  <div>
+  {recipeVariable}
+
+  </div>
+)
+
 }
 
-
-//
-// fetchAllUserData(){
-//   return fetch(`${url}users/1`)
-//   .then(res => res.json())
-//   .then(res =>
-//     this.setState({
-//     user: res
-//   }))
-// }
-
-  render(){
-    // console.log('user in my render',this.props.current_user)
-    // {this.state.user.current_user.recipes.map(recipe => {
-    //   recipe
-    // })}
-  return(
-    <div>
-
-
-    Hey guys recipe time
-
-    </div>
-  )
-}
-}
 
 const mapStateToProps = state => {
   // console.log('state.user in map state',state)
   return {
-    current_user: state.users.current_user
+    recipes: state.users.current_user.recipes
   }
 }
-
+  //
 
 export default withRouter(connect(mapStateToProps, actions)(MyRecipes))
